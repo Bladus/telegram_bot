07368950ac879882c96d8a0d98dd027fda3a1c0e
madiev <madiev@corp.sputnik.ru>
2019-06-03 18:56:01 +0300

    added parse

---
+++ src/application/funcs.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "gopkg.in/telegram-bot-api.v4"
+    "net/http"
+    "log"
+)
+
+func CreatePhoto(chat_id int64, link, caption string) tgbotapi.PhotoConfig {
+    photo := tgbotapi.NewPhotoShare(chat_id, link)
+    photo.Caption = caption
+
+    return photo
+}
+
+func request(path string) (*http.Response, error) {
+    client := &http.Client{}
+    log.Printf("[GET] %v\n", path)
+
+    req, err := http.NewRequest("GET", path, nil)
+    if err != nil {
+        log.Printf("ERROR http.NewRequest :: %v\n", err.Error())
+    }
+
+    return client.Do(req)
+}
+
+func check_error(err error) {
+    if err != nil {
+        log.Println(err)
+    }
+}
--- src/application/main.go
+++ src/application/main.go
@@ -4,10 +4,16 @@ import (
     "gopkg.in/telegram-bot-api.v4"
     "application/config"
     "encoding/json"
+    "io/ioutil"
+    "regexp"
     "flag"
     "fmt"
     "log"
     "os"
+)
+
+const (
+    TARGET = "https://ria.ru/photolents"
 )
 
 var c *config.Config
@@ -40,7 +46,54 @@ func main() {
 
     log.Printf("Authorized on account %s", bot.Self.UserName)
 
-    msg := tgbotapi.NewMessage(c.SendMessage, "test ping")
+    //msg := tgbotapi.NewMessage(c.SendMessage, "test ping")
 
-    bot.Send(msg)
+    //bot.Send(msg)
+
+    response, err := request(TARGET)
+    defer response.Body.Close()
+
+    if err != nil {
+        log.Printf("ERROR request(TARGET) :: %v\n", err.Error())
+    }
+
+    content, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        log.Printf("ERROR ioutil.ReadAll(response.Body) :: %v\n", err.Error())
+    }
+
+    re := regexp.MustCompile(`<a[^>]+\bhref=["']([^"']+\.html)" class="list-item__image">`)
+    items := re.FindAllStringSubmatch(string(content), -1)
+
+    stamp := items[0][1]
+
+    last, err := ioutil.ReadFile("./last.txt")
+    check_error(err)
+    log.Printf("last url :: %s\n", string(last))
+
+    if(string(last) == stamp) {
+        return
+    }
+
+    s := []byte(stamp)
+    err = ioutil.WriteFile("./last.txt", s, 0644)
+    check_error(err)
+
+    response2, err := request(stamp)
+    defer response2.Body.Close()
+    check_error(err)
+
+    content2, err := ioutil.ReadAll(response2.Body)
+    if err != nil {
+        log.Printf("ERROR ioutil.ReadAll(response2.Body) :: %v\n", err.Error())
+    }
+
+    re2 := regexp.MustCompile(`<img media-type="photo" alt="[^>]+" title="([^>]+)" src="([^>]+)">`)
+    images := re2.FindAllStringSubmatch(string(content2), -1)
+
+    for _, v := range images {
+        fmt.Printf("%s\n\n", v[1])
+        photo := CreatePhoto(c.SendMessage, v[2], v[1])
+        bot.Send(photo)
+    }
 }
