3ef1d543484c3c995d838380e50d5048a0b5f383
madiev <andrey_1_87@mail.ru>
2019-06-03 22:17:49 +0300

    test

--- src/application/main.go
+++ src/application/main.go
@@ -88,7 +88,7 @@ func main() {
         log.Printf("ERROR ioutil.ReadAll(response2.Body) :: %v\n", err.Error())
     }
 
-    re2 := regexp.MustCompile(`<img media-type="photo" alt="[^>]+" title="([^>]+)" src="([^>]+)">`)
+    re2 := regexp.MustCompile(`<img media-type="photo" alt="[^>]+" title="[^>]+" src="([^>]+)">`)
     images := re2.FindAllStringSubmatch(string(content2), -1)
 
     cap := regexp.MustCompile(`<div class="article__photo-item-text"><p>([^>]+)</p></div>`)
